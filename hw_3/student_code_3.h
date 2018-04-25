65d81e42b15d1c2e6cec6dccfdb50acce4218590
Timothy Mwiti <mwititimothyk@gmail.com>
2018-04-25 09:57:20 -0500

    Assignment completed: All problems solve correctly

--- hw_3/student_code_3.h
+++ hw_3/student_code_3.h
@@ -10,7 +10,6 @@
 //       via Canvas.
 //
 ///////////////////////////////////////////////////////////////////////////////
-
 //required libraries
 #include <vector>
 #include <utility>
@@ -23,7 +22,6 @@ struct IntervalNode {
 	int intervalNodeIndex;
 	IntervalNode(int startTime, int endTime, int intervalNodeIndex) : startTime(startTime), endTime(endTime), intervalNodeIndex(intervalNodeIndex) {}
 };
-
 
 struct end_time_key {
 	inline bool operator()(const IntervalNode &node1, const IntervalNode &node2) const {
@@ -47,7 +45,6 @@ std::vector<IntervalNode> initializeIntervalNodes(std::vector <std::pair<int, in
 }
 //you can include standard C++ libraries here
 
-
 // This function should return your name.
 // The name should match your name in Canvas
 
@@ -55,23 +52,42 @@ void GetStudentName(std::string& your_name)
 {
    //replace the placeholders "Firstname" and "Lastname"
    //with you first name and last name 
-   your_name.assign("Firstname Lastname");
+   your_name.assign("Timothy Mwiti");
 }
 
 //you can implement this function (recommended) or the the next one
 int FindBestSchedule (std::vector<std::pair<int,int>> availabilities, int T)
 {   
-   //your code goes here
+   //Vectors of IntervalNodes. First is sorted by start time, Second is sorted by end time
 	std::vector<IntervalNode> intervalsByStartTime = initializeIntervalNodes(availabilities);
 	std::vector<IntervalNode> intervalsByEndTime = initializeIntervalNodes(availabilities);
 	std::sort(intervalsByStartTime.begin(), intervalsByStartTime.end(), start_time_key());
 	std::sort(intervalsByEndTime.begin(), intervalsByEndTime.end(), end_time_key());
-   return -1; /* replace -1 with the correct value */
+    
+    //Indicate processed intervals (0 if not processed, 1 if processed)
+    int processedIntervals[availabilities.size()] = {0};
+    int intervalsCounter = 0;
+    while(intervalsByEndTime.size() > 0){
+        if(processedIntervals[intervalsByEndTime.front().intervalNodeIndex] == 1){
+            intervalsByEndTime.erase(intervalsByEndTime.begin() + 0);
+            continue;
+        }
+        intervalsCounter++;
+        int curEndTime = intervalsByEndTime.front().endTime;
+        int timeToCheckFor = curEndTime - T;
+        processedIntervals[intervalsByEndTime.front().intervalNodeIndex] = 1;
+        intervalsByEndTime.erase(intervalsByEndTime.begin() + 0);
+        while(intervalsByStartTime.size() > 0 && timeToCheckFor >= intervalsByStartTime[0].startTime){
+            processedIntervals[intervalsByStartTime[0].intervalNodeIndex] = 1;
+            intervalsByStartTime.erase(intervalsByStartTime.begin() + 0);
+        }
+    }
+   return intervalsCounter; /* replace -1 with the correct value */
 }
 
 //if you choose to implement this function, you can replace the code with 
 //your own code
-/*
+
 int FindBestSchedule (const std::vector<int>& left, const std::vector<int>& right, int T)
 {
    assert(left.size() == right.size());
@@ -87,4 +103,3 @@ int FindBestSchedule (const std::vector<int>& left, const std::vector<int>& righ
    
    return FindBestSchedule (availabilities, T);
 }
-*/
