5c87259e7581dd1cdff3a1d1c4f3eb1daf767bb6
Timothy Mwiti <mwititimothyk@gmail.com>
2018-05-27 15:58:44 -0500

    Fast dynamic algorithm done, greedy also included

--- hw_6/student_code_6.h
+++ hw_6/student_code_6.h
@@ -13,12 +13,11 @@
 
 //required libraries
 #include <string>
-#include <vector> 
-
-//you can include standard C++ libraries here
+#include <vector>
 #include <algorithm>
+#include <utility>
+#include<map>
 
-//std::map<std::string, int> weightsmap;
 // This function should return your name.
 // The name should match your name in Canvas
 
@@ -29,7 +28,57 @@ void GetStudentName(std::string& your_name)
    your_name.assign("Timothy Mwiti");
 }
 
-int FindMaxBalancedSequence (const std::vector<int>& weights)
+
+std::vector<std::vector<int>> computedValues;
+void initializeVectors(int size){
+    std::vector<int> tempVect(size*2 - 1, -2147483647);
+    for(int i = 0; i < size; i++){
+        computedValues.push_back(tempVect);
+    }
+}
+
+void FindMaxBalancedSequenceHelperDynamic(const std::vector<int>& weights){
+    int weightsSize = weights.size();
+    for(int i = weightsSize-1; i >= 0; i--){
+        for(int j = (weightsSize-1)-i; j <= (weightsSize-1)+i; j++){
+            if(i == weightsSize-1){
+                computedValues[i][weightsSize-1] = 0;
+                computedValues[i][weightsSize-2] = weights[i];
+                computedValues[i][weightsSize] = -weights[i];
+                break;
+            }else{
+                bool by_neg = computedValues[i+1][j-1] == -2147483647;
+                bool by_zero = computedValues[i+1][j] == -2147483647;
+                bool by_one = computedValues[i+1][j+1] == -2147483647;
+                if(!by_neg && by_zero && by_one){
+                    computedValues[i][j] = computedValues[i+1][j-1]-weights[i];
+                }else if(by_neg && !by_zero && by_one){
+                    computedValues[i][j] = computedValues[i+1][j];
+                }else if(by_neg && by_zero && !by_one){
+                    computedValues[i][j] = computedValues[i+1][j+1] + weights[i];
+                }else if(!by_neg && !by_zero && by_one){
+                    computedValues[i][j] = std::max(computedValues[i+1][j-1]-weights[i], computedValues[i+1][j]);
+                }else if(!by_neg && by_zero && !by_one){
+                    computedValues[i][j] = std::max(computedValues[i+1][j-1]-weights[i], computedValues[i+1][j+1] + weights[i]);
+                }else if(by_neg && !by_zero && !by_one){
+                    computedValues[i][j] = std::max(computedValues[i+1][j], computedValues[i+1][j+1] + weights[i]);
+                }else if(!by_neg && !by_zero && !by_one){
+                    int tempMax = std::max(computedValues[i+1][j], computedValues[i+1][j+1] + weights[i]);
+                    computedValues[i][j] = std::max(tempMax, computedValues[i+1][j-1]-weights[i]);
+                }else{
+                    computedValues[i][j] = -2147483647;
+                }                
+            }
+        }
+    }
+
+}
+
+/*
+    THIS IS AN IMPLEMENTATION OF A GREEDY APPROACH. ASSIGNMENT RUNS USING DYNAMIC BUT A USER
+    CAN RUN USING THIS APPROACH IF THEY PREFER TO.
+*/
+int FindMaxBalancedSequenceGreedy (const std::vector<int>& weights)
 {
     std::vector<int> weightscopy = weights;
     std::sort(std::begin(weightscopy), std::end(weightscopy), std::greater<int>());
@@ -39,4 +88,13 @@ int FindMaxBalancedSequence (const std::vector<int>& weights)
         sum += std::max((weightscopy.at(i) - weightscopy.at(weights_size-(i+1))), 0);
     }
     return sum;
+}
+
+
+int FindMaxBalancedSequence (const std::vector<int>& weights)
+{
+    computedValues.clear();
+    initializeVectors(weights.size());
+    FindMaxBalancedSequenceHelperDynamic(weights);   
+    return computedValues[0][computedValues[0].size()/2];
 }
